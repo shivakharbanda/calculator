b7aa3a8f8739fa0d341d7755d29bda3785bfea97
shivakharbanda <shivakharbanda11@gmail.com>
2022-09-20 22:42:57 +0530

    version 5

--- logic.js
+++ logic.js
@@ -1,8 +1,9 @@
+let display = "0";
+let result = null
+let operand1 = null;
+let operand2 = null;
+let operatorId = null;
 let operator = "inactive";
-let result = "0";
-let operand1 = 0;
-let operand2 = "none";
-let operatorId = 0;
 
 calculate();
 
@@ -19,249 +20,142 @@ makeBtnWork(nine);
 makeBtnWork(zero);
 
 ac.addEventListener("click", () => {
-    operator = "inactive";
-    result = "0";
-    operand1 = 0;
-    operand2 = "none";
-    operatorId = 0;
-    result = 0;
+    display = "0";
+    result = null
+    operand1 = null;
+    operand2 = null;
+    operatorId = null;
     calculate()
 });
 
 function calculate() {
+    result = display;
     if (result == undefined) {
         return
     }
 
     calcResult = document.querySelector("#result");
     calcResult.textContent = `${result}`;
-}
-
-
-
-function makeBtnWork(btn) {
-    btn.addEventListener("click", (e) => {
-        ////console.log((operator));
-        if (result == 0) {
-            result = btn.textContent;
-            
-        } else if (result == "none" || result == undefined){
-            result = 0 + Number(btn.textContent);
-        }
-         else if (result != 0) {
-            result = result + btn.textContent;
-        };
-        calculate();
-    });   
 };
 
-plus.addEventListener("click", (e) =>{
-    eq();
-
-    
-
-    console.log(operand1);
-    console.log(operand2);
-
-    operatorId = e.target.id;
-    if (operator == "active" && result == "none") {
-        //////console.log("@")
-        
-    } else if (operator == "active" && result != "none") {
-        operand2 = result;
-        result = add(operand1, operand2);
-        calculate();
-        operator = "inactive";
-
-    } else if (operator == "inactive") {
-        operator = "active";
-        operand1 = result;
-        result = "none";
-
-    }
-
-    
-    
-});
-
-minus.addEventListener("click", (e) =>{
-
-    eq();
-
-    operatorId = e.target.id;
-
-    if (operator == "active" && result == "none") {
-        //console.log("@")
-        
-    } else if (operator == "active" && result != "none") {
-        operand2 = result;
-        result = subtract(operand1, operand2);
-        calculate();
-        operator = "inactive";
-
-    } else if (operator == "inactive") {
-        operator = "active";
-        operand1 = result;
-        result = "none";
-
-    }
-    //console.log(result);
-    //console.log(operand1);
-    //console.log(operand2);
-    
-});
-
-equals.addEventListener("click", () => {
-    operand2 = calcResult.textContent;
-    //console.log((operatorId));
-    // minus button id is 11
-    if (parseInt(operatorId) == 11 && operator == "active") {
-        result = subtract(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-    } else if (parseInt(operatorId) == 15 && operator == "active") {     
-        // plus btn id is 15
-
-        result = add(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    } else if (parseInt(operatorId) == 7 && operator == "active") {     
-        // multiply btn id is 7
-
-        result = product(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    } else if (parseInt(operatorId) == 3 && operator == "active") {     
-        // divide btn id is 3
-
-        result = divideAbyB(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    }
-
-
-});
-
-multiply.addEventListener("click", (e) =>{
-    eq();
-    operatorId = e.target.id;
-    if (operator == "active" && result == "none") {
-        //console.log("@")
-        
-    } else if (operator == "active" && result != "none") {
-        operand2 = result;
-        result = product(operand1, operand2);
-        calculate();
-        operator = "inactive";
-
-    } else if (operator == "inactive") {
-        operator = "active";
-        operand1 = result;
-        result = "none";
-
-    }
-
-    //console.log(operand1);
-    //console.log(operand2);
-    
-});
-
-divide.addEventListener("click", (e) =>{
-    
-    eq();
-    operatorId = e.target.id;
-
-    if (operator == "active" && result == "none") {
-        //console.log("@")
-        
-    } else if (operator == "active" && result != "none") {
-        operand2 = result;
-        result = divideAbyB(operand1, operand2);
-        calculate();
-        operator = "inactive";
-
-    } else if (operator == "inactive") {
-        operator = "active";
-        operand1 = result;
-        result = "none";
-
-    };
-    
-});
-
-
-function eq() {
-    operand2 = calcResult.textContent;
-    //console.log((operatorId));
-    // minus button id is 11
-    if (parseInt(operatorId) == 11 && operator == "active") {
-        result = subtract(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-    } else if (parseInt(operatorId) == 15 && operator == "active") {     
-        // plus btn id is 15
-
-        result = add(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    } else if (parseInt(operatorId) == 7 && operator == "active") {     
-        // multiply btn id is 7
-
-        result = product(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    } else if (parseInt(operatorId) == 3 && operator == "active") {     
-        // divide btn id is 3
-
-        result = divideAbyB(operand1, result);
-        calculate();
-        operator = "inactive";
-        operand1 = 0;
-        operand2 = "none";
-        //console.log(result);
-
-    }  else if (parseInt(operatorId) == 1 && operator == "active") {
-        operand1 = Number(result);
-        result = sqr(operand1);
-        calculate()
-        operand1 = 0;
-        operand2 = "none";
-    } else if (parseInt(operatorId) == 2 && operator == "active") {
-        operand1 = Number(result);
-        result = centage(operand1);
-        calculate()
-        operand1 = 0;
-        operand2 = "none";
-    }
+function changeDisplay(result) {
+    calcResult = document.querySelector("#result");
+    calcResult.textContent = `${result}`;
 }
 
+function btnWork(e) {
+    display = document.querySelector("#result").textContent;
+    let splitDisplay = display.split(".");
+    if (splitDisplay.length == 2 && splitDisplay[1] != "" && operator == "inactive") {
+        console.log("here");
+        return;
+    }
+    if (Number(display) == 0 || operator == "active") {
+        display = e.target.textContent;
+        operator = "inactive";
+    } else {
+        display += e.target.textContent;
+    };
+    calculate()
+};
 
 
+function makeDotWork(e) {
+    //console.log(e);
+    display = document.querySelector("#result").textContent;
+    let splitDisplay = display.split(".");
+    let splitDisplaySize = splitDisplay.length;
 
+    if (splitDisplaySize == 1) {
+        if (Number(display) == 0 || operator == "active") {
+            display = e.target.textContent;
+            operator = "inactive";
+        } else {
+            display += e.target.textContent;
+        };
+        calculate()
+    } else if (splitDisplaySize > 1) {
+        return;
+    };
+};
+
+decimal.addEventListener("click", makeDotWork);
+
+function makeBtnWork(btn) {
+    btn.addEventListener("click", btnWork);   
+};
+
+
+function operate(operatorId, operand1, operand2) {
+
+    if (operatorId == 15) {
+        result = add(operand1, operand2);
+        console.log(result);
+        changeDisplay(result);
+        operatorId = null
+    } else if (operatorId == 11) {
+        result = subtract(operand1, operand2);
+        console.log(result);
+        changeDisplay(result);
+        operatorId = null
+    } else if (operatorId == 7) {
+        result = product(operand1, operand2);
+        console.log(result);
+        changeDisplay(result);
+        operatorId = null
+    } else if (operatorId == 3) {
+        if (operand1 == 0 && operand2 == 0) {
+            result = "MATH ERROR";
+            changeDisplay("MATH ERROR");
+            result = 0;
+            return;
+        };
+        result = divideAbyB(operand1, operand2);
+        console.log(result);
+        changeDisplay(result);
+        operatorId = null
+    };
+
+};
+
+plus.addEventListener("click", (e) => {
+    operate(operatorId, operand1, operand2)
+    operand1 = display;
+    operatorId = e.target.id;
+    operator = "active";
+
+});
+
+minus.addEventListener("click", (e) => {
+    operand1 = display;
+    operatorId = e.target.id;
+    operator = "active";
+
+});
+
+multiply.addEventListener("click", (e) => {
+    operand1 = display;
+    operatorId = e.target.id;
+    operator = "active";
+
+});
+
+
+divide.addEventListener("click", (e) => {
+    operand1 = display;
+    operatorId = e.target.id;
+    operator = "active";
+
+});
+
+equals.addEventListener("click", (e) => calc(e));
+
+function calc(e) {
+    operand2 = display;
+    if (operatorId) {
+        operate(operatorId, operand1, operand2);
+    };
+};
 
 function add() {
     if (arguments[1] == "none") {
